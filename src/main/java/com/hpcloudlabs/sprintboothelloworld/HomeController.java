9a84ea7d58f2254c4572cc03f73764ba272debc7
Harry Sidhu <hpsiddhu@gmail.com>
2025-01-24 11:37:28 -0500

    Initial Commit - Sample HelloWorld Spring Application

---
+++ src/main/java/com/hpcloudlabs/sprintboothelloworld/HomeController.java
@@ -0,0 +1,13 @@
+package com.hpcloudlabs.sprintboothelloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/hpcloudlabs/sprintboothelloworld/SpringbootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpcloudlabs.sprintboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
